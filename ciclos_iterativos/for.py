c69e3d17d43d69100786de7248e97f81d20035cd
Danny Jimenez <danny.jimenez@profesores.uamerica.edu.co>
2023-12-13 16:39:42 -0500

    Se adiciono prueba.py y una carpeta con el acrhivo fot.py

---
+++ ciclos_iterativos/for.py
@@ -0,0 +1,2 @@
+for value in range(0,10,1):
+    print(value)
---
+++ prueba.py
@@ -0,0 +1,6 @@
+num = int(input("ingrese un numero cualquiera :"))
+
+if num%2 == 0:
+    print(f"{num} es un numero par")
+else:
+    print((f"{num} es un numero impar"))

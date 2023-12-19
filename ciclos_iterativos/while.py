7514c0a6c4df4f7275185bbba837e1b41d997f63
“Danny <da.jimenezm@uniandes.edu.co>
2023-12-18 23:39:23 -0500

    agregar imagen messi

--- ciclos_iterativos/while.py
+++ ciclos_iterativos/while.py
@@ -1,6 +1,14 @@
+from PIL import Image
+
+
 cont = 0
 while cont != 101: 
     print(cont)
     cont = cont + 1
+
+#mostrar una imagen cualquiera 
+    
+img = Image.open("D:\Documentos Danny\Platzi\Git y Github\proyecto1\ciclos_iterativos\imagen.jpeg")
+img.show()
 
 
